6e031ee922cb5442f5ae55259defe15373afd838
Johnny <johnathan.mnn@gmail.com>
2023-10-04 15:16:40 -0500

    Solved NExtAuth error

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -2,27 +2,25 @@
 import Link from 'next/link';
 import Image from 'next/image';
 import {useState, useEffect} from 'react';
-import {signIn, signOut, useSession, getProvider} from 'next-auth/react';
+import {signIn, signOut, useSession, getProviders} from 'next-auth/react';
 
 const Nav = () => {
+    const {data: session} = useSession();
     const [providers, setProviders] = useState(null);
 
-    useEffect(()=>{
-        const setProviders = async () =>{
-            const response = await getProviders();
+    useEffect(() => {
+        (async () => {
+          const res = await getProviders();
+          setProviders(res);
+        })();
+      }, []);
 
-            setProviders(response);
-        }
-
-        setProviders();
-    }, [])
-const isUserLoggedIn = true;
   return (
     <nav>
         <h1>Nav</h1>
         <Link href='/'>Home</Link>
-        {isUserLoggedIn ? (
-            <h1>Logged In</h1>
+        {session?.user ? (
+            <button type='button' onClick={signOut}>Sign out</button>
         ):(
             <>
             {providers &&
